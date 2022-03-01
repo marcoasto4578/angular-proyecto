95d97c783ff43d267f002c3b704ee5d0c8c9fbc4
Marco <marcoasto123@gmail.com>
2022-03-01 13:22:52 -0500

    aumentar amigo en contador

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './contador.component.html',
 })
 export class ContadorComponent{
+  amig:string="amigo"
   title:string="Contador App"
   numero:number=0;
   base:number=2;
